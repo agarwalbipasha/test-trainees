3aacd0aa62a6f6275256df77f19a964fd035310c
Bipasha Agarwal <agarwalbipasha@gmail.com>
2023-06-06 21:54:02 +0530

    Added the provision for leaves

--- index.js
+++ index.js
@@ -6,64 +6,80 @@ const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect(`mongodb+srv://agarwalbipasha:vWOj28rB7YHK1ojM@cluster0.td1hcft.mongodb.net/`);
+mongoose.connect(
+  `mongodb+srv://agarwalbipasha:vWOj28rB7YHK1ojM@cluster0.td1hcft.mongodb.net/`
+);
 
 const database = mongoose.connection;
 
 database.on("error", (error) => {
-    console.log(error);
-})
+  console.log(error);
+});
 
 database.once("connected", () => {
-    console.log("Successfully connected to Mongoose");
-})
+  console.log("Successfully connected to Mongoose");
+});
 
 app.get("/", async (req, res) => {
-    const allTrainees = await Trainee.find({});
-    try {
-        res.json(allTrainees);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+  const allTrainees = await Trainee.find({});
+  try {
+    res.json(allTrainees);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.post("/", async (req, res) => {
-    const newTrainee = new Trainee({
-        name: req.body.name,
-        id: req.body.id,
-        email: req.body.email
-    });
-    try {
-        const data = await newTrainee.save();
-        res.status(200).json(data);
-    } catch(error) {
-        res.status(400).json({message: error.message});
-    }
+  const newTrainee = new Trainee({
+    name: req.body.name,
+    id: req.body.id,
+    email: req.body.email,
+    leavesHalfDay: req.body.leavesHalfDay,
+    leavesFullDay: req.body.leavesFullDay
+  });
+  try {
+    const data = await newTrainee.save();
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.delete("/delete/:id", async (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    try {
-        const data = await Trainee.findByIdAndDelete(id);
-        res.send(`Deleted ${data}`);
-    } catch(error) {
-        res.status(500).json({ message: error.message});
-    }
+  try {
+    const data = await Trainee.findByIdAndDelete(id);
+    res.send(`Deleted ${data}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.patch("/update/:id", async (req, res) => {
-    const id = req.params.id;
-    const updatedTrainee = req.body;
+  const id = req.params.id;
+  const updatedTrainee = {
+    name: req.body.name,
+    id: req.body.id,
+    email: req.body.email,
+  }
 
-    try {
-        const result = await Trainee.findByIdAndUpdate(id, updatedTrainee);
-        res.json("Data updated successfully");
-    } catch(error) {
-        res.status(400).json({message: error.message});
-    }
+  if (Array.isArray(req.body.leavesHalfDay)) {
+    updatedTrainee.$addToSet = {leavesHalfDay: {$each: req.body.leavesHalfDay}}
+  };
+
+  if (Array.isArray(req.body.leavesFullDay)) {
+    updatedTrainee.$addToSet = {leavesFullDay: {$each: req.body.leavesFullDay}}
+  }
+
+  try {
+    const result = await Trainee.findByIdAndUpdate(id, updatedTrainee);
+    res.json(`Data updated successfully: ${result}`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log("Server connected successfully");
-})
+  console.log("Server connected successfully");
+});
--- model.js
+++ model.js
@@ -1,32 +1,58 @@
 const mongoose = require("mongoose");
 
-const validateEmail = function(email) {
-    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return regex.test(email);
+const validateEmail = function (email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
 };
-  
 
 const traineeSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: true,
-        trim: true,
-        minLength: 1
+  id: {
+    type: Number,
+    required: true,
+    trim: true,
+    minLength: 1,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: [validateEmail, "Please enter a valid email ID."],
+  },
+  leavesHalfDay: {
+    type: [String],
+    validate: {
+      validator: function (value) {
+        const isValid = value.every((date) => {
+          const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
+          return dateRegex.test(date);
+        });
+        return isValid;
+      },
+      message: "Invalid date format",
     },
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 3,
-        maxlength: 20
+    
+  },
+  leavesFullDay: {
+    type: [Date],
+    validate: {
+      validator: function (value) {
+        const isValid = value.every((date) => {
+          const dateRegex = /^\d{2}-\d{2}-\d{4}$/;
+          return dateRegex.test(date);
+        });
+        return isValid;
+      },
+      message: "Invalid date format",
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        validate: [validateEmail, "Please enter a valid email ID."]
-    }
+  },
 });
 
 const Trainee = mongoose.model("Trainee", traineeSchema);
