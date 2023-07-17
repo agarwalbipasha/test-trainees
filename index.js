bbf94ba7e425b14a4c82af18e5a83075ff1a7d56
Bipasha Agarwal <agarwalbipasha@gmail.com>
2023-07-17 18:59:39 +0530

    Rectified the error for path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const morgan = require('morgan');
 const fs = require("fs");
+const path = require('path');
 const port = process.env.PORT;
 
 app.use(express.json());
